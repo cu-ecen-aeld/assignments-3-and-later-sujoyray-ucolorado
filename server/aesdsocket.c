fdf8735f90184b586d48a06ebc911fe7a592bd1c
Sujoy Ray <sujoy.ray@colorado.edu>
2023-02-25 20:58:09 -0800

    Restructure file
    Tested
    Tested locally using full-test.sh
    
    Signed-off-by: Sujoy Ray <sujoy.ray@colorado.edu>

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,8 +1,26 @@
 
-/* Inspired by
-https://www.geeksforgeeks.org/socket-programming-cc/
+/*****************************************************************************
+* Copyright (C) 2023
+*
+* Redistribution, modification or use of this software in source or binary
+* forms is permitted as long as the files maintain this copyright. Users are
+* permitted to modify this and use it to learn about the field of embedded
+* software. Sujoy Ray and the University of Colorado are not liable for
+* any misuse of this material.
+*
+*****************************************************************************/
+/**
+* @file aesdsocket.c
+* @brief This file has aesdsocket socket implementations
+*
+* @author Sujoy Ray 
+* @date February 25, 2023
+* @version 1.0
+* CREDIT: Header credit: University of Colorado coding standard
+* CREDIT: Inspired by https://www.geeksforgeeks.org/socket-programming-cc
+* CREDIT: Consulted stackoverflow 
+*
 */
-
 
 #include <stdio.h>
 #include <sys/socket.h>
@@ -18,9 +36,9 @@ https://www.geeksforgeeks.org/socket-programming-cc/
 #include <time.h>
 
 
-
-
-
+/*******************************************************************************
+ * Definitions
+*******************************************************************************/
 #define SOCKET_PORT                 (9000)
 #define MAX_SERVER_CONNECTION       (1)
 #define TRUE                        (1)
@@ -28,55 +46,78 @@ https://www.geeksforgeeks.org/socket-programming-cc/
 #define PACKET_TIMEOUT_END          (10)
 #define MSEC_2_USEC(x)              ((x) * 1000)
 #define FIXED_RD_BUF_SIZE           (1024)
-volatile sig_atomic_t exit_aesd_soc = FALSE;
-volatile sig_atomic_t file_close = TRUE;
 
-void process_and_save_data(char *buffer, char *file_buffer, int value_read, int *wr_pointer, FILE *fp) {
-    int buf_rd_ptr = 0;    
-    int pos = 0;    
-    char *start = &buffer[buf_rd_ptr];
-    char *end = &buffer[buf_rd_ptr];
-    int len_to_copy = value_read;  
-    end  = strstr(buffer, "\n");
-    while(1) {
-        if(end == NULL) {
-            memcpy(&file_buffer[*wr_pointer], &buffer[buf_rd_ptr], len_to_copy);
-            *wr_pointer += len_to_copy;
-            buf_rd_ptr = 0;
-            break;
-        }
-        else {
-            pos = (unsigned int)(end - start+1);
-            memcpy(&file_buffer[*wr_pointer], &buffer[buf_rd_ptr], pos);  
-            *wr_pointer += pos;
-            file_buffer[*wr_pointer] ='\n';            
-            fwrite (file_buffer , sizeof(char), *wr_pointer, fp);
-            fflush(fp);
-            *wr_pointer = 0;
-            len_to_copy = len_to_copy - pos;
-            buf_rd_ptr += pos;
-            if(len_to_copy == 0)
-            break;
-        }
-        start = end+1;
-        end  = strstr(start, "\n");
-    }
+/*******************************************************************************
+ * Prototypes
+*******************************************************************************/
+static void process_and_save_data(char *buffer, char *file_buffer, 
+    int value_read, int *wr_pointer, FILE *fp);
 
-    printf("value_read = %d and len_to_copy =%d \n", value_read, len_to_copy);
-}
+static void aesdsoc_sighandler(int signal_no);
+
+static void process_and_save_data(char *buffer, char *file_buffer, 
+    int value_read, int *wr_pointer, FILE *fp);
+
+static int aesdsocket_server(int d_mode); 
 
 
-void aesdsoc_sighandler(int signal_no) {
-    if (signal_no == SIGINT || signal_no == SIGTERM) {
-       exit_aesd_soc = TRUE;
-       syslog(LOG_INFO, "Caught signal, exiting"); 
-    }
-    if (signal_no == SIGINT )
-       file_close = FALSE;     
+/*******************************************************************************
+ * Variables and Macros
+*******************************************************************************/
+static volatile sig_atomic_t exit_aesd_soc = FALSE;
+static volatile sig_atomic_t file_close = TRUE;
 
-}
+
+
+/*******************************************************************************
+ * Code
+*******************************************************************************/
+
+
+/*
+* Application entry point
+* 
+* Parameters:
+*   argc: Argument count
+*   argv: Argument vector
+*
+* Returns: 0 if the function executed without any error. Otherwise, error code 
+*          is logged in syslog and the program exists with code 1
+*   
+*/
 int main(int argc, char **argv)
 {
+    int d_mode = 0;
+    /* Open log and set log level */
+    openlog ("aesdsocket", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_USER);
+    setlogmask (LOG_UPTO (LOG_DEBUG));
+
+    if(argc > 1) {
+        if (strcmp(argv[1],"-d") !=0) {
+            syslog(LOG_INFO,"-d is NOT detected \n");
+            d_mode = 0;
+        }
+        else {     
+            d_mode = 1;            
+            syslog(LOG_INFO,"-d is detected \n");
+        }
+    }
+    aesdsocket_server(d_mode);
+    return 0;
+    
+}
+
+/*
+* Socket server API
+* 
+* Parameters:
+*   d_mode: Flag stating if daemon mode is enabled or not, 1= daemon mode
+*
+* Returns: 0 if the function executed without any error. Otherwise, error code 
+*          is logged in syslog and the program exists with code non zero value
+*   
+*/
+static int aesdsocket_server(int d_mode) {
     int soc_server = -1;
     int soc_client = -1;
     int rc = -1;
@@ -95,40 +136,24 @@ int main(int argc, char **argv)
     struct timespec now;
     int diff_time = 0;
 
-    /* Open log and set log level */
-    openlog ("aesdsocket", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_USER);
-    setlogmask (LOG_UPTO (LOG_DEBUG));
+    if(d_mode == TRUE)
+        daemon(0,0);
 
-    if(argc > 1)
-    {
-        if (strcmp(argv[1],"-d") !=0)
-        {
-            printf("%d %s \n",argc, argv[1]);
-            printf("-d is NOT detected \n");
-        }
-        else
-        {        
-            printf("-d is detected \n");
-            daemon(0,0);
-        }
-   }
-
-   
-   fp =  fopen("/var/tmp/aesdsocketdata", "a+");
-   if (fp == NULL) {
+    fp =  fopen("/var/tmp/aesdsocketdata", "a+");
+    if (fp == NULL) {
            printf("Can't open file");
            exit (1);
-   }
+    }
 
-   file_close = TRUE;
-   
-   syslog(LOG_INFO, "Sujoy: Starting APP"); 
+    file_close = TRUE;
 
-   memset(&signal_action, 0, sizeof(signal_action));
-   signal_action.sa_handler = aesdsoc_sighandler;
+    syslog(LOG_INFO, "Sujoy: Starting APP"); 
 
-   sigaction(SIGINT,  &signal_action, 0);
-   sigaction(SIGTERM, &signal_action, 0);
+    memset(&signal_action, 0, sizeof(signal_action));
+    signal_action.sa_handler = aesdsoc_sighandler;
+
+    sigaction(SIGINT,  &signal_action, 0);
+    sigaction(SIGTERM, &signal_action, 0);
 
 
 
@@ -237,12 +262,12 @@ int main(int argc, char **argv)
                 buffer = (char*)realloc(buffer, buffer_size);
             }
         }
-    
-   }
 
-exit_shutdown:
+    }
+
+    exit_shutdown:
     shutdown(soc_server, SHUT_RDWR);
-return_func:
+    return_func:
     if(file_close)
     {
         fclose(fp);
@@ -251,7 +276,73 @@ return_func:
         printf("Error file removal\n");
     }
     closelog();
+    rc = (exit_aesd_soc == TRUE) ? 0 : rc;
     return rc;
+}
 
+/*
+* process_and_save_data
+* 
+* Parameters:
+*   buffer:         Pointer to data buffer containing client data. 
+*                   Freed by the caller.
+*   file_buffer:    Pointer to file buffer, freed by the caller.
+*   length:         Size of data buffer in bytes
+*   write_ptr:      Index of file buffer
+*   fp:             File pointer
+*
+* Returns: None
+*/
+static void process_and_save_data(char *buffer, char *file_buffer, 
+    int length, int *wr_pointer, FILE *fp) {
+
+    int buf_rd_ptr = 0;    
+    int pos = 0;    
+    char *start = &buffer[buf_rd_ptr];
+    char *end = &buffer[buf_rd_ptr];
+    int len_to_copy = length;  
+    end  = strstr(buffer, "\n");
+    while(1) {
+        if(end == NULL) {
+            memcpy(&file_buffer[*wr_pointer], &buffer[buf_rd_ptr], len_to_copy);
+            *wr_pointer += len_to_copy;
+            buf_rd_ptr = 0;
+            break;
+        }
+        else {
+            pos = (unsigned int)(end - start+1);
+            memcpy(&file_buffer[*wr_pointer], &buffer[buf_rd_ptr], pos);  
+            *wr_pointer += pos;
+            file_buffer[*wr_pointer] ='\n';            
+            fwrite (file_buffer , sizeof(char), *wr_pointer, fp);
+            fflush(fp);
+            *wr_pointer = 0;
+            len_to_copy = len_to_copy - pos;
+            buf_rd_ptr += pos;
+            if(len_to_copy == 0)
+            break;
+        }
+        start = end+1;
+        end  = strstr(start, "\n");
+    }
+
+}
+
+/*
+* aesdsoc_sighandler
+* 
+* Parameters:
+*   signal_no:      Signal number. 
+*
+* Returns: None
+*/
+static void aesdsoc_sighandler(int signal_no) {
+
+    if (signal_no == SIGINT || signal_no == SIGTERM) {
+       exit_aesd_soc = TRUE;
+       syslog(LOG_INFO, "Caught signal, exiting"); 
+    }
+    if (signal_no == SIGINT )
+       file_close = FALSE;     
 }
 
